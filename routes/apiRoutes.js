3f339d8b08b4d2dc11661ff692088cae9d01f580
unknown <jacobchrisb@gmail.com>
2021-07-31 12:05:25 -0400

    package.json

---
+++ routes/apiRoutes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const db = require("../models");
+// show all
+router.get("/api/workouts", (req, res) => {
+  console.log("Displaying all workouts");
+  db.Workout.find({})
+    .then((results) => {
+      console.log(results);
+      res.json(results);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// add 
+router.put("/api/workouts/:id", (req, res) => {
+  console.log("Add a new workout below");
+  db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body }}, {new: true} )
+    .then((results) => {
+      console.log(results);
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+
+      res.json(err);
+    });
+});
+
+// create
+router.post("/api/workouts", (req, res) => {
+  console.log("design your new workout below");
+  db.Workout.create(req.body)
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//display a range different workouts
+router.get("/api/workouts/range", (req, res) => {
+  console.log("workout range");
+  db.Workout.find({})
+    .then((results) => {
+      console.log(results);
+      res.json(results);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const routes = require("./routes/apiRoutes");
+const path = require('path')
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "html");
+
+
+app.use(logger("dev"));
+app.use(routes);
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+    
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
